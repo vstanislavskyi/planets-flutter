91fce471cfbfa0eec2f2350d3067e3ec698a9b46
V.Stanislavskyi <vstanislavskyi@gmail.com>
2018-03-18 09:44:42 +0200

    adding content to the card

--- lib/ui/home/HomePageBody.dart
+++ lib/ui/home/HomePageBody.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:elon_musk/model/planets.dart';
 import 'package:elon_musk/ui/home/PlanetRow.dart';
 
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new PlanetRow();
+    return new PlanetRow(planets[0]);
   }
 }
--- lib/ui/home/PlanetRow.dart
+++ lib/ui/home/PlanetRow.dart
@@ -1,9 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:elon_musk/model/planets.dart';
 
 class PlanetRow extends StatelessWidget {
+  final Planet planet;
+
+  PlanetRow(this.planet);
+
   @override
   Widget build(BuildContext context) {
-    var planetCard = new Container(
+    final baseTextStyle = const TextStyle(
+      fontFamily: 'Poppins',
+    );
+
+    final headerTextStyle = baseTextStyle.copyWith(
+      color: Colors.white,
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+    );
+
+    final regularTextStyle = baseTextStyle.copyWith(
+      color: const Color(0xffb6b2df),
+      fontSize: 9.0,
+      fontWeight: FontWeight.w400,
+    );
+
+    final subHeaderTextStyle = regularTextStyle.copyWith(fontSize: 12.0);
+
+    final planetCardContent = new Container(
+      margin: new EdgeInsets.fromLTRB(76.0, 16.0, 16.0, 16.0),
+      constraints: new BoxConstraints.expand(),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            height: 4.0,
+          ),
+          new Text(
+            planet.name,
+            style: headerTextStyle,
+          ),
+          new Container(
+            height: 10.0,
+          ),
+          new Text(
+            planet.location,
+            style: subHeaderTextStyle,
+          ),
+          new Container(
+            margin: new EdgeInsets.symmetric(vertical: 8.0),
+            height: 2.0,
+            width: 18.0,
+            color: new Color(0xFF00C6FF),
+          ),
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                child: _planetValue(
+                  value: planet.distance,
+                  valueStyle: regularTextStyle,
+                  image: "assets/img/ic_distance.png",
+                ),
+              ),
+              new Expanded(
+                child: _planetValue(
+                  value: planet.gravity,
+                  valueStyle: regularTextStyle,
+                  image: "assets/img/ic_gravity.png",
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+
+    final planetCard = new Container(
       height: 124.0,
       margin: const EdgeInsets.only(left: 46.0),
       decoration: new BoxDecoration(
@@ -16,13 +87,15 @@ class PlanetRow extends StatelessWidget {
                 blurRadius: 10.0,
                 offset: new Offset(0.0, 10.0))
           ]),
+      child: planetCardContent,
     );
 
     final planetThumbnail = new Container(
       margin: new EdgeInsets.symmetric(vertical: 16.0),
       alignment: FractionalOffset.centerLeft,
       child: new Image(
-        image: new AssetImage("assets/img/mars.png"),
+        //image: new AssetImage("assets/img/mars.png"),
+        image: new AssetImage(planet.image),
         width: 92.0,
         height: 92.0,
       ),
@@ -46,6 +119,18 @@ class PlanetRow extends StatelessWidget {
           planetThumbnail,
         ],
       ),
+    );
+  }
+
+  Widget _planetValue({value, image, valueStyle}) {
+    return new Row(
+      children: <Widget>[
+        new Image.asset(image, height: 12.0),
+        new Container(
+          width: 8.0,
+        ),
+        new Text(value, style: valueStyle),
+      ],
     );
   }
 }

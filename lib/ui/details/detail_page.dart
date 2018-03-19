cf9c4781fee293980fd8645ab8e3ab9ec3de12fb
V.Stanislavskyi <vstanislavskyi@gmail.com>
2018-03-19 06:58:33 +0200

    planet detail page (in progress)

--- lib/ui/details/detail_page.dart
+++ lib/ui/details/detail_page.dart
@@ -10,21 +10,54 @@ class DetailPage extends StatelessWidget {
     return new Scaffold(
         body: new Container(
       constraints: new BoxConstraints.expand(),
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      color: new Color(0xFF736AB7),
+      child: new Stack(
         children: <Widget>[
-          new Text(planet.name),
-          new Hero(
-            tag: "planet-hero-${planet.id}",
-            child: new Image(
-              //image: new AssetImage("assets/img/mars.png"),
-              image: new AssetImage(planet.image),
-              width: 92.0,
-              height: 92.0,
-            ),
-          )
+          _getBackground(),
+          _getGradient(),
+          _getContent(),
+          _getToolbar(context),
         ],
       ),
     ));
+  }
+
+  Widget _getBackground() {
+    return new Container(
+      child: new Image.network(
+        planet.picture,
+        fit: BoxFit.cover,
+        height: 300.0,
+      ),
+      constraints: new BoxConstraints.expand(height: 300.0),
+    );
+  }
+
+  Widget _getGradient() {
+    return new Container(
+      margin: new EdgeInsets.only(top: 190.0),
+      height: 110.0,
+      decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+        colors: <Color>[new Color(0x00736AB7), new Color(0xFF736AB7)],
+        stops: [0.0, 0.9],
+        begin: const FractionalOffset(0.0, 0.0),
+        end: const FractionalOffset(0.0, 1.0),
+      )),
+    );
+  }
+
+  Widget _getToolbar(BuildContext context) {
+    return new Container(
+      margin: new EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: new BackButton(color: Colors.white),
+    );
+  }
+
+  Widget _getContent() {
+    return new ListView(
+      padding: new EdgeInsets.fromLTRB(0.0, 72.0, 0.0, 32.0),
+      children: <Widget>[],
+    );
   }
 }
